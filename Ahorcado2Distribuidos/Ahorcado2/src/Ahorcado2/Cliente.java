10409a57430b9ef2deb29df41ea937b7ce647f9e
seayala <55987311+seayala@users.noreply.github.com>
2022-12-29 18:08:04 +0100

    Update Cliente.java
    Signed-off-by: seayala <55987311+seayala@users.noreply.github.com>

--- Ahorcado2Distribuidos/Ahorcado2/src/Ahorcado2/Cliente.java
+++ Ahorcado2Distribuidos/Ahorcado2/src/Ahorcado2/Cliente.java
@@ -2,47 +2,73 @@ package Ahorcado2;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.*;
 
-//Para que el servidor conecte a los clientes
-public class HiloServidor extends Thread{
-	private Socket s;
-	private List<DatosUsuario> jugadores;
-	//private CountDownLatch count;
-	
-	public HiloServidor(Socket s, List<DatosUsuario> jugadores) {
-		super();
-		this.s = s;
-		this.jugadores = jugadores;
-		//this.count = count;
-	}
-	
-	public void run() {
-		try(DataOutputStream dos = new DataOutputStream(s.getOutputStream())){
-			
-			this.jugadores.add(new DatosUsuario(this.s.getInetAddress().toString(), this.s.getPort()));
-			//this.count.countDown();
-			//Enviamos
-			for(DatosUsuario du : this.jugadores) {
-				dos.writeBytes(du.getHost() + " ? " + du.getPort() + "\r\n");
-				dos.flush();
+public class Cliente {
+	public void soyServidor(int puerto) {
+		ExecutorService pool = Executors.newFixedThreadPool(3);
+		try(ServerSocket servidor = new ServerSocket(puerto)){
+			while(true) {
+				try {
+					Socket s = servidor.accept();
+					pool.submit(new HiloCliente(puerto));
+				}
+				catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		finally {
-			try {
-				this.s.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	}
+	public void soyJugador(String host, int puerto) {
+		try(Socket jugador = new Socket(host, puerto)){
+			
+			
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
-	
+	public static void main(String[] args) {
+		List<DatosUsuario> jugadores = new ArrayList<>();
+		try(Socket s = new Socket("localhost", 6666);
+			DataInputStream dis = new DataInputStream(s.getInputStream())){
+			
+			String jugador = dis.readLine();
+			while(jugador != null) {
+				String split[] = jugador.split(" ? ");
+				if(!s.getLocalAddress().toString().equals(split[0])) {
+					jugadores.add(new DatosUsuario(split[0], Integer.parseInt(split[2])));
+				}
+				jugador = dis.readLine();
+			}
+			
+			//Mostrar la lista
+			int indice = 1;
+			for(DatosUsuario user : jugadores) {
+				System.out.println(indice + "- " + user.toString());
+				indice++;
+			}
+			
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
+
 
