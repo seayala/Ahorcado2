01683bda7f6477a9af375d1646e78c02735c1935
seayala <55987311+seayala@users.noreply.github.com>
2022-12-29 18:04:35 +0100

    Add files via upload
    Signed-off-by: seayala <55987311+seayala@users.noreply.github.com>

---
+++ Ahorcado2Distribuidos/Ahorcado2/src/Ahorcado2/DatosUsuario.java
@@ -0,0 +1,35 @@
+package Ahorcado2;
+
+public class DatosUsuario {
+	private String host;
+	private int port;
+	
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public DatosUsuario(String host, int port) {
+		super();
+		this.host = host;
+		this.port = port;
+	}
+
+	@Override
+	public String toString() {
+		return "Direccion IP: " + host + ", puerto: " + port;
+	}
+	
+	
+}
