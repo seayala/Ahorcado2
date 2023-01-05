4c4b51fbe4855d973da3753d511c7d65175c1191
seayala <55987311+seayala@users.noreply.github.com>
2023-01-05 16:34:13 +0100

    Update HiloCliente.java
    Signed-off-by: seayala <55987311+seayala@users.noreply.github.com>

--- Ahorcado2Distribuidos/Ahorcado2/src/Ahorcado2/HiloCliente.java
+++ Ahorcado2Distribuidos/Ahorcado2/src/Ahorcado2/HiloCliente.java
@@ -1,69 +1,99 @@
 package Ahorcado2;
 
 import java.io.*;
-import java.io.IOException;
 import java.net.*;
+import java.nio.charset.Charset;
 import java.util.*;
 import java.util.concurrent.*;
 
 //Para el cliente que ponga la palabra, actua como servidor
-public class HiloCliente implements Runnable{
-	private int puerto;
+public class HiloCliente implements Runnable {
 	private Socket s;
 	private List<String> hanSalido;
 	private String palabra;
-	private Respuesta r;
-	
-	public HiloCliente(int puerto, String palabra, Respuesta r) {
+	private List<Character> letras = new ArrayList<>();
+
+	public HiloCliente(Socket s, String palabra) {
 		super();
-		this.puerto = puerto;
-		this.hanSalido=new ArrayList<String>();
-		this.palabra=palabra;
-		this.r=r;
+		this.s = s;
+		this.hanSalido = new ArrayList<String>();
+		this.palabra = palabra;
+		// Troceamos la palabra
+		for (int i = 0; i < palabra.length(); i++) {
+			this.letras.add(palabra.charAt(i));
+		}
 	}
+
+	private static List<Integer> contieneLetra(List<Character> letras, Character letra) {
+		List<Integer> posiciones = new ArrayList<>();
+		for (int i = 0; i < letras.size(); i++) {
+			if (letras.get(i).equals(letra)) {
+				posiciones.add(i);
+			}
+		}
+		return posiciones;
+	}
+
 	@Override
-	public void run() {	
-		while(r.getNumFallos()<11) {
-		try(BufferedReader in= new BufferedReader(new InputStreamReader(s.getInputStream()));ObjectOutputStream out= new ObjectOutputStream(s.getOutputStream());BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-			System.out.println("Introduce la letra o la palabra que consideres adecuada");
-			String intento=br.readLine();
-			if(hanSalido.contains(intento)) {
-				System.out.println("La palabra o letra introducida ya ha sido elegida con anterioridad");
-			}else {	
-				if(intento.length()==1) {
-					if(palabra.contains(intento)) {
+	public void run() {
+		Respuesta resp;
+		try (BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(this.s.getOutputStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(this.s.getInputStream()));
+			ObjectOutputStream out = new ObjectOutputStream(this.s.getOutputStream())) {
+			
+			// Enviamos el tamaño de la palabra
+			String tamaño = palabra.length() + "\r\n";
+			bfw.write(tamaño);
+			bfw.flush();
+
+			Respuesta r;
+			int numFallos = 0;
+			int salida = palabra.length();
+			// Juego
+			do {
+				r = new Respuesta();
+				r.setNumFallos(numFallos);
+				String intento = in.readLine();
+				r.setIntento(intento);
+				if (hanSalido.contains(intento)) {
+					r.setEsta(false);
+					r.setPosiciones(new ArrayList<>());
+					// No se actualiza el número de fallos.
+				} else if (intento.length() == 1) {
+					Character letra = intento.charAt(0);
+					List<Integer> posiciones = contieneLetra(letras, letra);
+					if (posiciones.size() == 0) {
+						r.setEsta(false);
+						r.setNumFallos(r.getNumFallos() + 1);
+						r.setPosiciones(new ArrayList<>());
+					} else {
 						r.setEsta(true);
-						for (int i = 0;i < palabra.length(); i++){
-							String letra=Character.toString(palabra.charAt(i)); //te separa la palabra del master en letras
-							if(letra.equals(intento)) { 
-								r.setLetraPosicion(i); //añade la posicion de la letra en la palabra (empieza en 0!!)
-							}
-						}
-					}else {
-						r.setNumFallos(r.getNumFallos()+1);
+						r.setPosiciones(posiciones);
+						salida -= r.getPosiciones().size();
 					}
-					
-				}else {
-					if(palabra.equals(intento)) {
+				} else { // El intento es una palabra
+					if (palabra.equals(intento)) {
 						r.setEsta(true);
-						r.setNumFallos(13); //imposible llegar a 13 fallos si no has ganando la partida
-						//JUEGO ACABADO
-					}else {
-						r.setIntento(r.getIntento()+2);
+						r.setNumFallos(28); // Para en el cliente comprobarlo
+						r.setPosiciones(new ArrayList<>());
+					} else {
+						r.setEsta(false);
+						r.setNumFallos(r.getNumFallos() + 2);
+						r.setPosiciones(new ArrayList<>());
 					}
 				}
+
+				// Enviamos el objeto
 				out.writeObject(r);
 				out.flush();
-			}
-		
-			
+				hanSalido.add(intento);
+				numFallos = r.getNumFallos();
+			} while (r.getNumFallos() < 11 && salida != 0);
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		}
 	}
-	
-	
 
 }
