f727a3d11d2cf3163502aac9f3bca36e1e15d1f9
seayala <55987311+seayala@users.noreply.github.com>
2023-01-05 16:35:33 +0100

    Update Servidor.java
    Signed-off-by: seayala <55987311+seayala@users.noreply.github.com>

--- Ahorcado2Distribuidos/Ahorcado2/src/Ahorcado2/Servidor.java
+++ Ahorcado2Distribuidos/Ahorcado2/src/Ahorcado2/Servidor.java
@@ -11,9 +11,6 @@ import java.util.concurrent.*;
 public class Servidor {
     public static void main(String[] args) {
     	List<DatosUsuario> usuarios = Collections.synchronizedList(new ArrayList<>());
-		usuarios.add(new DatosUsuario("192.168.1.1", 25));
-		usuarios.add(new DatosUsuario("192.168.1.2", 258));
-		usuarios.add(new DatosUsuario("192.168.1.3", 25896));
     	ExecutorService pool = Executors.newFixedThreadPool(4);
     	//final CountDownLatch cont = new CountDownLatch(4);
         try(ServerSocket servidor = new ServerSocket(6666)){
